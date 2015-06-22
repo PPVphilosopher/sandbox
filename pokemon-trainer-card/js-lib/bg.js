4c4027fba74ea4f883cdc679874b9f49beaae67d
ppvphilosopher <ppvphilosopher@github.com>
2015-06-22 10:24:33 +0700

    add more 5 monotype bg

--- pokemon-trainer-card/js-lib/bg.js
+++ pokemon-trainer-card/js-lib/bg.js
@@ -17,6 +17,108 @@ var bg_top = {
 		"url_mini": "mono-dragon-mini.png",
 		"overlay": "d2.png",
 	},
+	// "mono-fire": {
+	// 	"name": "Mono type - Fire",
+	// 	"url": "mono-fire.png",
+	// 	"url_mini": "mono-fire-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-grass": {
+	// 	"name": "Mono type - Grass",
+	// 	"url": "mono-grass.png",
+	// 	"url_mini": "mono-grass-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-water": {
+	// 	"name": "Mono type - Water",
+	// 	"url": "mono-water.png",
+	// 	"url_mini": "mono-water-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	"mono-ground": {
+		"name": "Mono type - Ground",
+		"url": "mono-ground.png",
+		"url_mini": "mono-ground-mini.png",
+		"overlay": "d2.png",
+	},
+	"mono-electric": {
+		"name": "Mono type - Electric",
+		"url": "mono-electric.png",
+		"url_mini": "mono-electric-mini.png",
+		"overlay": "d2.png",
+	},
+	// "mono-bug": {
+	// 	"name": "Mono type - Bug",
+	// 	"url": "mono-bug.png",
+	// 	"url_mini": "mono-bug-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	"mono-psychic": {
+		"name": "Mono type - Psychic",
+		"url": "mono-psychic.png",
+		"url_mini": "mono-psychic-mini.png",
+		"overlay": "d2.png",
+	},
+	// "mono-rock": {
+	// 	"name": "Mono type - Rock",
+	// 	"url": "mono-rock.png",
+	// 	"url_mini": "mono-rock-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-poison": {
+	// 	"name": "Mono type - Poison",
+	// 	"url": "mono-poison.png",
+	// 	"url_mini": "mono-poison-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-flying": {
+	// 	"name": "Mono type - Flying",
+	// 	"url": "mono-flying.png",
+	// 	"url_mini": "mono-flying-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-normal": {
+	// 	"name": "Mono type - Normal",
+	// 	"url": "mono-normal.png",
+	// 	"url_mini": "mono-normal-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-ghost": {
+	// 	"name": "Mono type - Ghost",
+	// 	"url": "mono-ghost.png",
+	// 	"url_mini": "mono-ghost-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-fighting": {
+	// 	"name": "Mono type - Fighting",
+	// 	"url": "mono-fighting.png",
+	// 	"url_mini": "mono-fighting-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	// "mono-ice": {
+	// 	"name": "Mono type - Ice",
+	// 	"url": "mono-ice.png",
+	// 	"url_mini": "mono-ice-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	"mono-steel": {
+		"name": "Mono type - Steel",
+		"url": "mono-steel.png",
+		"url_mini": "mono-steel-mini.png",
+		"overlay": "d2.png",
+	},
+	// "mono-dark": {
+	// 	"name": "Mono type - Dark",
+	// 	"url": "mono-dark.png",
+	// 	"url_mini": "mono-dark-mini.png",
+	// 	"overlay": "d2.png",
+	// },
+	"mono-fairy": {
+		"name": "Mono type - Fairy",
+		"url": "mono-fairy.png",
+		"url_mini": "mono-fairy-mini.png",
+		"overlay": "d2.png",
+	},
 }
 
 var bg_bottom = {
