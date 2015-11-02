ccc3945b86718564b53c9700e75ca2100d5a44a0
ppvphilosopher <ppvphilosopher@github.com>
2015-11-03 00:11:46 +0700

    ip

--- pokemon-trainer-card/ip.js
+++ pokemon-trainer-card/ip.js
@@ -1,24 +1,19 @@
 function myIP() {
-    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    $.get("http://api.hostip.info/get_html.php",false, function (data) {
+        data = data.toString().replace(/ /g, '').trim();
+        data = data.split('\n');
 
-    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
-    xmlhttp.send();
+        var text = ''
+        $.each(data, function (key, val) {
+            var x = val.split(':');
+            if (x[1] && x[1].indexOf('Unknown') == -1) {
+                if (text) text += ',';
+                text += x[1];
+            }
+        });
 
-    var data = xmlhttp.responseText;
-    data = data.toString().replace(/ /g, '').trim();
-    data = data.split('\n');
-
-    var text = ''
-    $.each(data, function (key, val) {
-        var x = val.split(':');
-        if (x[1] && x[1].indexOf('Unknown') == -1) {
-            if (text) text += ',';
-            text += x[1];
-        }
+        return text;
     });
-
-    return text;
 }
 
 var ip_data = myIP();
